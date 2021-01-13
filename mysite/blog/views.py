d78bd3fcf319c24bfc15f77378f8581821382e29
HmurBolotny <djtestch98@gmail.com>
2021-01-13 14:29:45 +0500

    прикрутил e-mail

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
+
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -9,9 +12,10 @@ class PostListView(ListView):
     paginate_by = 3
     template_name = 'blog/post/list.html'
 
+
 def post_list(request):
     object_list = Post.published.all()
-    paginator = Paginator(object_list, 3)# отображение по 3 записи
+    paginator = Paginator(object_list, 3)               # отображение по 3 записи
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
@@ -21,9 +25,36 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
+
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year,
                              publish__month=month, publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'djtestch89@gmail.com', [cd['to']])
+            sent = True
+
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
+
 
 # Create your views here.
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'djtestch89@gmail.com'
+EMAIL_HOST_PASSWORD = 'testdj98'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
