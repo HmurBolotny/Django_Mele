64e1c7d4a5aee59c6d17c187862ac9e5fd1aae2a
HmurBolotny <djtestch98@gmail.com>
2021-01-20 15:35:36 +0500

    рабочая карта сайта

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import sys
+sys.path
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
-from ..mysite.blog.sitemaps import PostSitemap
+from blog.sitemaps import PostSitemap
 
 
 sitemaps = {'posts': PostSitemap, }
